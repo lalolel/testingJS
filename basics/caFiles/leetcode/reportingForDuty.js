93b1c188baea98cff0dd5167e3d58e5dad8beaa2
Lalo <114935993+lalolel@users.noreply.github.com>
2025-06-17 23:59:00 +0300

    Create reportingForDuty.js

---
+++ basics/caFiles/leetcode/reportingForDuty.js
@@ -0,0 +1,17 @@
+/* Write a function, reportingForDuty(), that has two string parameters, rank and lastName, and returns a string in the following format: ‘rank lastName reporting for duty!’
+
+reportingForDuty('Private', 'Fido') 
+// Should return 'Private Fido reporting for duty!'
+ */
+
+const reportingForDuty = (rank, lastName) => `${rank} ${lastName} reporting for duty!`
+
+/*
+// Using string concatenation:
+const reportingForDuty = (rank, lastName) => rank + " " + lastName + " " + "reporting for duty!"
+
+// As a function declaration:
+function reportingForDuty(rank, lastName) {
+    return `${rank} ${lastName} reporting for duty!`
+}
+*/
