fc4d2ee8b58350241800a166779062a9f85d424d
Lalo <114935993+lalolel@users.noreply.github.com>
2025-06-19 05:03:27 +0300

    Create greetAliens.js

---
+++ basics/caFiles/leetcode/greetAliens.js
@@ -0,0 +1,45 @@
+/* Write a function, greetAliens(), that takes in an array of strings and uses a for loop to print a greeting with each string in the array.
+
+The greeting should take the following format:
+“Oh powerful [stringElement], we humans offer our unconditional surrender!”
+
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+greetAliens(aliens);
+// Should print:
+// Oh powerful Blorgous, we humans offer our unconditional surrender! 
+// Oh powerful Glamyx, we humans offer our unconditional surrender! 
+// Oh powerful Wegord, we humans offer our unconditional surrender! 
+// Oh powerful SpaceKing, we humans offer our unconditional surrender! 
+
+Copy to Clipboard
+
+Note: You may have noticed how convenient it would be to use .forEach(), but you’re not allowed to use it here. Don’t worry you’ll have plenty of opportunities to use built-in methods later on!
+ */
+const greetAliens = arr => {
+      for (let i = 0; i < arr.length; i++) {
+            console.log('Oh powerful ' + arr[i] + ', we humans offer our unconditional surrender!');
+      }
+}
+
+/*
+// Alternate solutions:
+
+// Using string interpolation
+const greetAliens = arr
+for (let i = 0; i<arr.length; i++){
+    console.log(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!`);
+}
+
+// As a function declaration:
+function greetAliens(arr) {
+for (let i = 0; i<arr.length; i++){
+    console.log(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!`);
+}
+}
+
+*/
+
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+greetAliens(aliens)
